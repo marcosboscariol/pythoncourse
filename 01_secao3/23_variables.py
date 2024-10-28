6da980a35446f89bf4f729d3662caee9ec51ae21
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-10-28 17:58:28 -0300

    add file

---
+++ 01_secao3/23_variables.py
@@ -0,0 +1,6 @@
+# Variáveis são utilizadas para salvar algo a mem'pria do computador
+# PEP 8: Iniciar as variáveis com letras minúsculas
+# Exemplo: nome_variavel
+
+nome_compleo = 'Marcos Boscariol'
+print(nome_compleo)
