d18104e9f5502b3a2a3d7d13aadafb79de57f500
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-10-28 15:23:53 -0300

    add file

---
+++ 01_secao3/19_string.py
@@ -0,0 +1,14 @@
+# Strings são textos que estão dentro de aspas
+# str -> string -> texto
+
+# Aspas simples
+print('Texto com aspas simples')
+
+# Aspas duplas
+print("Texto com aspas duplas")
+
+# Escape
+print("Texto com \"aspas duplas\"")
+
+# r
+print(r"Texto com \"aspas duplas\"")
